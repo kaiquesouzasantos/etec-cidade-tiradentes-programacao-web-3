b0bcac31963e23b9d98ca26a2b76dac6153d5d3f
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-02-21 07:44:07 -0300

    Delete exercicios/01/assets/js/storage directory

--- exercicios/01/assets/js/storage/listaPessoas.js
+++
@@ -1,9 +0,0 @@
-import { Pessoa } from "../model/Pessoa"
-
-export const LISTA_PESSOAS = [
-    new Pessoa('Miguel', 15, 60, 1.65),
-    new Pessoa('Paulo', 17, 70, 1.72),
-    new Pessoa('Joao', 29, 60, 1.80),
-    new Pessoa('Marcos', 17, 70, 1.69),
-    new Pessoa('Lucas', 32, 110, 1.95)
-]
