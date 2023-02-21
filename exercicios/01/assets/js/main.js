2d1cacd9a4641d65a98b916c32b2740a07e952a6
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-02-21 08:03:13 -0300

    Update main.js

--- exercicios/01/assets/js/main.js
+++ exercicios/01/assets/js/main.js
@@ -22,17 +22,17 @@ const LISTA_PESSOAS = [
 
 function retornaEstadoIMC(imc){
     if (imc < 18.5) {
-        return "Abaixo do peso!";
+        return "Abaixo do   Peso!";
     } else if (imc >= 18.5 && imc < 25) {
-        return "Peso normal!";
+        return "Peso Normal!";
     } else if (imc >= 25 && imc < 30) {
         return "Sobrepeso!";
     } else if (imc >= 30 && imc < 35) {
-        return "Obesidade grau I !";
-    } else if (pessoa.IMC >= 35 && imc < 40) {
-        return "Obesidade grau II !";
+        return "Obesidade Grau I !";
+    } else if (imc >= 35 && imc < 40) {
+        return "Obesidade Grau II !";
     } else {
-        return "Obesidade grau III !";
+        return "Obesidade Grau III !";
     }
 }
 
