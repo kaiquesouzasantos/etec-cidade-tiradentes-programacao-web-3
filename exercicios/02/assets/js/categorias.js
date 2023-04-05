361a3bced1fd46c73417e949c6665e8c3874a80c
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-04-04 22:00:20 -0300

    Delete exercicios/02 directory

--- exercicios/02/assets/js/categorias.js
+++
@@ -1,8 +0,0 @@
-export let CATEGORIAS = {
-    abaixoDoPeso:0,
-    pesoNormal:0,
-    sobrepeso:0,
-    obesidade1:0,
-    obesidade2:0,
-    obesidade3:0
-}
--- exercicios/02/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-import { ManipuladorHTML } from './manipuladorHTML.js'
-import { LISTA_PESSOAS } from './listaPessoas.js'
-
-document.addEventListener('mouseover', () => {
-    LISTA_PESSOAS
-        .forEach(pessoa => ManipuladorHTML.apresentaInformacoes(pessoa))
-        ManipuladorHTML.apresentaInformacoesSumarizadas()
-})
